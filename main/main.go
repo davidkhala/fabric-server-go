bce3fac40fa49c19d9ab297e9f05861d57640206
David Liu <david-khala@hotmail.com>
2023-12-14 11:51:37 +0800

    add icon

--- main/main.go
+++ main/main.go
@@ -15,19 +15,20 @@ import (
 // @contact.email david-khala@hotmail.com
 func main() {
 
-	App := restful.Run(true)
-	App.GET("/", restful.Ping)
+	app := restful.Run(true)
+	app.StaticFile("/favicon.ico", "./favicon.ico")
+	app.GET("/", restful.Ping)
 
-	App.POST("/fabric/ping", gateway.PingFabric)
-	App.POST("/fabric/create-proposal", gateway.CreateProposal)
-	App.POST("/fabric/transact/process-proposal", gateway.ProcessProposal)
-	App.POST("/fabric/transact/commit", gateway.Commit)
-	App.POST("/ecosystem/create-token", CreateToken)
-	App.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler)) // refers to /swagger/*any
+	app.POST("/fabric/ping", gateway.PingFabric)
+	app.POST("/fabric/create-proposal", gateway.CreateProposal)
+	app.POST("/fabric/transact/process-proposal", gateway.ProcessProposal)
+	app.POST("/fabric/transact/commit", gateway.Commit)
+	app.POST("/ecosystem/create-token", CreateToken)
+	app.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler)) // refers to /swagger/*any
 
 	port, exists := os.LookupEnv("PORT")
 	if !exists {
 		port = "8080"
 	}
-	goutils.PanicError(App.Run(":" + port))
+	goutils.PanicError(app.Run(":" + port))
 }
